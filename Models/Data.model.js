b646dd290056bcbdceedab68c31fa25faedeacc9
Kristianne10 <kristianneblessy@gmail.com>
2022-12-03 00:35:48 +0800

    answer and data

---
+++ Models/Data.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema({
+    autoCreate: false,
+    title: {
+        type: String,
+        required: true,
+    },
+    questions: [
+        {
+        question: "",
+        comments: [
+            {
+            comment: "",
+            }
+        ]
+        }
+    ]
+});
+
+const Data = mongoose.model('data', DataSchema);
+module.exports = Data;
--- Routes/Form.route.js
+++ Routes/Form.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
-const Form = require('../Models/Form.model')
+const Form = require('../Models/Form.model');
+const Data = require('../Models/Data.model');
+const Answer = require('../Models/Answer.model');
 
 router.post('/forms', async (req, res, next) => {
 
@@ -27,6 +29,47 @@ router.get('/ListOfForms', async (req, res, next) => {
         next(error);
     }
 
+});
+
+
+
+
+router.post('/input', async (req, res, next) => {
+    try {
+        const data = new Data(req.body)
+        const savedData = await data.save()
+        
+        res.send({savedData})
+    } catch (error){
+        next(error);
+    }
+
+});
+
+router.post('/answer', async (req, res, next) => {
+    try {
+        const answer = new Answer(req.body)
+        const savedAnswer = await answer.save()
+        
+        res.send({savedAnswer})
+    } catch (error){
+        next(error);
+    }
+
+});
+
+
+router.post('/title', async (req, res, next) => {
+    try {
+        const FormTitle = (req.body.wtitle)
+        const FormList = await Data.findOne({title: FormTitle})
+        res.send(JSON.stringify(FormList))
+        console.log(FormList)
+    } catch (error){
+        next(error);
+    }
+
 })
+
 
 module.exports = router;
